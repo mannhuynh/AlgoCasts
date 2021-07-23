f4b7d987b40c3d5058d01ce57549460cc3783192
Vuong Huynh <mannhuynh@gmail.com>
2021-07-23 08:48:48 -0500

    Add reverseint and fizzbuzz challenges

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -1,3 +1,4 @@
+/*
 // --- Directions
 // Given an integer, return an integer that is the reverse
 // ordering of numbers.
@@ -7,7 +8,30 @@
 //   reverseInt(500) === 5
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
-
-function reverseInt(n) {}
-
+*/
+function reverseInt(n) {
+	let n_str = n.toString();
+	// console.log(n_str);
+	if (n < 0) {
+		let reverse_str = "-";
+		for (let i = n_str.length - 1; i > 0; i--) {
+			reverse_str += n_str[i];
+			// console.log(reverse_str);
+		}
+		return parseInt(reverse_str);
+	}
+	return parseInt(n_str.split("").reverse().join(""));
+}
+console.log(reverseInt(4523000));
+console.log(reverseInt(-321000));
 module.exports = reverseInt;
+
+// function reverseInt(n) {
+//     const reversed = n
+//       .toString()
+//       .split('')
+//       .reverse()
+//       .join('');
+
+//     return parseInt(reversed) * Math.sign(n);
+//   }
